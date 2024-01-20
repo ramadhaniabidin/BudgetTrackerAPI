aa5a457f6b997fd9956c1626529f23c7666388a5
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-01-20 14:22:27 +0700

    Update ExpenseController

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,23 @@
+﻿using Logics.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("BudgetTrackerAPI/{controller}/{action}")]
+    public class ExpenseController : Controller
+    {
+        private IExpenseRepository _repo;
+        public ExpenseController(IExpenseRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllExpense()
+        {
+            var obj = _repo.GetAllExpenses();
+            return Ok(obj);
+        }
+    }
+}
